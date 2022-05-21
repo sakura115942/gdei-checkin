86238c58d8a5a4aa398b1d7a5392142461ce4248
chan <dd115942@gmail.com>
2022-05-21 12:35:24 +0800

    add password encrypt

--- checkin.py
+++ checkin.py
@@ -2,8 +2,11 @@ import os
 import json
 import time
 import logging
+import base64
 
 import requests
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pksc1_v1_5
+from Crypto.PublicKey import RSA
 
 requests.packages.urllib3.disable_warnings()
 
@@ -28,13 +31,20 @@ params = {
 accounts = json.loads(os.environ['ACCOUNTS'])
 passwords = json.loads(os.environ['PASSWORDS'])
 
+def encrpt(password, public_key=None):
+    if public_key is None:
+        public_key = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaGxxomLHQmUzALgHx3tV0L/zY0fabQJVPWJeW0O+KBsQdeP/HC015zV6C4ExiBxHkA8RtnSfMkKKBH3/6dyatFCrCxIw5KgtNqmC9dtsdUmDJq8IDW7hFkODAyzDAvuovvGn+wQvRl97iEqUVstnLi7SD9zCuM39AUM4ugtqsBwIDAQAB'
+    rsakey = RSA.importKey(public_key)
+    cipher = Cipher_pksc1_v1_5.new(rsakey)
+    cipher_text = base64.b64encode(cipher.encrypt(password.encode()))
+    return cipher_text.decode()
 
 if __name__ == '__main__':
     logger.info('start')
 
     for k, v in zip(accounts, passwords):
         form['username'] = k
-        form['password'] = v
+        form['password'] = encrpt(v)
         params = {'_': int(float(time.time())*1000)}
         session = requests.session()
         session.headers = { "User-Agent": "hello" }
